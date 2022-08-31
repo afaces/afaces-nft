d072bba6e72006e961a3fd84a5c60039b2e101d7
Axlfc <axelfernandezcurros@gmail.com>
2022-08-31 18:00:57 +0200

    UPDATED: Cleaning some code and adding special metadata

--- stable/painters.py
+++ stable/painters.py
@@ -1,29 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-spain_url = "https://en.wikipedia.org/wiki/List_of_Spanish_painters"
-res = requests.get(spain_url)
-html_page = res.content
 
-soup = BeautifulSoup(html_page, 'html.parser')
-text = soup.find_all(text=True)
-output = ''
-blacklist = [
-    '[document]',
-    'noscript',
-    'header',
-    'html',
-    'meta',
-    'head',
-    'input',
-    'script',
-    # there may be more elements you don't want, such as "style", etc.
-]
-for t in text:
-    if t.parent.name not in blacklist:
+def scrapeSiteText(url):
+    res = requests.get(url)
+    html_page = res.content
+    soup = BeautifulSoup(html_page, 'html.parser')
+    text = soup.find_all(text=True)
+    output = ''
+    blacklist = [
+        '[document]',
+        'noscript',
+        'header',
+        'html',
+        'meta',
+        'head',
+        'input',
+        'script',
+        # there may be more elements you don't want, such as "style", etc.
+    ]
+    for t in text:
+        if t.parent.name not in blacklist:
+            output += '{} '.format(t)
+    return output
+
+
+def getRawText(url):
+    res = requests.get(url)
+    html_page = res.content
+    soup = BeautifulSoup(html_page, 'html.parser')
+    return soup.find_all(text=True)
+
+
+def getGitHubRawTextURL(owner, name, path, branch):
+    return "https://raw.githubusercontent.com/" + owner + "/" + name + "/" + branch + "/" + path
+
+
+def formatList(text):
+    output = ''
+    for t in text:
         output += '{} '.format(t)
+    return output.split('\n')
 
-lines = str(output.split('\n')).split('Contents:')[1][200:20050].split('\n')
+
+lines = str(scrapeSiteText("https://en.wikipedia.org/wiki/List_of_Spanish_painters").split('\n')).split('Contents:')[1][200:20050].split('\n')
 lines = lines[0].split(" ', ' ")[:-1]
 
 spanishPainters = []
@@ -32,33 +52,7 @@ for i in lines:
         continue
     spanishPainters.append(i)
 
-'''for painter in spanishPainters:
-    print(painter)
-'''
-
-url = "https://en.wikipedia.org/wiki/100_Great_Paintings"
-res = requests.get(url)
-html_page = res.content
-
-soup = BeautifulSoup(html_page, 'html.parser')
-text = soup.find_all(text=True)
-output = ''
-blacklist = [
-    '[document]',
-    'noscript',
-    'header',
-    'html',
-    'meta',
-    'head',
-    'input',
-    'script',
-    # there may be more elements you don't want, such as "style", etc.
-]
-for t in text:
-    if t.parent.name not in blacklist:
-        output += '{} '.format(t)
-
-lines3 = str(output.split('\n')).split('Contents:')[0]
+lines3 = str(scrapeSiteText("https://en.wikipedia.org/wiki/100_Great_Paintings").split('\n')).split('Contents:')[0]
 lines3 = lines3.split(" ', '")[62:268]
 
 britishFamousPaintings = []
@@ -68,13 +62,12 @@ for i in lines3:
         if i % 2:
             britishFamousPaintings.append(text[i].split("(")[0])
 
-for painting in britishFamousPaintings:
-    print(painting)
 
-print(len(britishFamousPaintings)) # 213 titles
-
-print(len(spanishPainters))
+artists, flavors, mediums, movements = formatList(getRawText(getGitHubRawTextURL("pharmapsychotic", "clip-interrogator", "data/artists.txt", "main"))),\
+                                       formatList(getRawText(getGitHubRawTextURL("pharmapsychotic", "clip-interrogator", "data/flavors.txt", "main"))),\
+                                       formatList(getRawText(getGitHubRawTextURL("pharmapsychotic", "clip-interrogator", "data/mediums.txt", "main"))),\
+                                       formatList(getRawText(getGitHubRawTextURL("pharmapsychotic", "clip-interrogator", "data/movements.txt", "main")))
 
 for britishFamousPainting in britishFamousPaintings:
     for spanishPainter in spanishPainters:
-        print(britishFamousPainting + ", by " + spanishPainter)
+        print(britishFamousPainting[:-2] + " by " + spanishPainter)
